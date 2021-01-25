e6316066ab7e559fe260519314432dfc5ab95bbf
Luiz Faleiros <luiz.faleiros@iteris.com.br>
2021-01-25 20:54:03 -0300

    auth

---
+++ src/app/account/shared/auth-token.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthTokenService {
+  public getToken(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  public decodePayloadJWT(): any {
+    try {
+      return jwt_decode(this.getToken());
+    } catch (Error) {
+      return null;
+    }
+  }
+}
