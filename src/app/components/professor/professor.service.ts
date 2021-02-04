6cec468891285ca6f3f80632cde601c68d695fc1
Luiz Faleiros <luiz.faleiros@iteris.com.br>
2021-02-04 19:16:14 -0300

    Arrumando professores

--- src/app/components/professor/professor.service.ts
+++ src/app/components/professor/professor.service.ts
@@ -1,3 +1,4 @@
+import { AuthTokenService } from './../../account/shared/auth-token.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -12,7 +13,7 @@ import { PersonType } from '../aluno/person-type-enum';
 })
 export class ProfessorService {
 
-  constructor(private snackBar: MatSnackBar, private http: HttpClient ) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient, private tokenService: AuthTokenService ) { }
 
   showMessage(msg: string): void {
     this.snackBar.open(msg, 'X', {
@@ -24,28 +25,28 @@ export class ProfessorService {
 
   get(): Observable<Professor[]>  {
     return this.http.get<Professor[]>(`${environment.urls.personList}?person-type= ${PersonType.PROFESSOR}`, {
-      headers: new HttpHeaders().append('Authorization', this.gettoken())
+      headers: new HttpHeaders().append('Authorization', this.tokenService.sendToken())
     });
   }
 
   create(person: Professor): Observable<Professor> {
     return this.http.post<Professor>(environment.urls.personSave, person, {
-      headers: new HttpHeaders().append('Authorization', this.gettoken())
+      headers: new HttpHeaders().append('Authorization', this.tokenService.sendToken())
     });
   }
 
   getById(id: number): Observable<Professor>{
     const url = `${environment.urls.personById}/${id}`;
     return this.http.get<Professor>(url, {
-      headers: new HttpHeaders().append('Authorization', this.gettoken())
+      headers: new HttpHeaders().append('Authorization', this.tokenService.sendToken())
     });
-    
+
   }
 
   update(person: Professor): Observable<Professor>{
     const url = `${environment.urls.personUpdate}/${person.id}`;
     return this.http.put<Professor>(url, person, {
-      headers: new HttpHeaders().append('Authorization', this.gettoken())
+      headers: new HttpHeaders().append('Authorization', this.tokenService.sendToken())
     });
   }
 
@@ -53,12 +54,7 @@ export class ProfessorService {
     console.log(professor.id);
     const url = `${environment.urls.personDelete}/${professor.id}`;
     return this.http.delete<Professor>(url, {
-      headers: new HttpHeaders().append('Authorization', this.gettoken())
+      headers: new HttpHeaders().append('Authorization', this.tokenService.sendToken())
     });
   }
-
-  gettoken(): string {
-    return localStorage.getItem("token") || '';
-  }
-
 }
